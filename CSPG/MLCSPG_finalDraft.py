a5f419e365d37c1a25e69ffeaeefc947fcf52499
Jean-Luc Bouchot <jlbouchot@gmail.com>
2016-08-08 15:21:50 +0200

    Uploaded first version of the tests for the final version of the MLCSPG paper

---
+++ CSPG/MLCSPG_finalDraft.py
@@ -0,0 +1,11 @@
+from test_diff_avg_v_ML import Main as DIFF_V
+from test_piecewiselineardiffusion_v import Main as PWLD_V # As in PieceWiseLinearDiffusion
+from test_piecewiselineardiffusion_v_2D import Main as PWLD_2D
+from test_diff_avg_v_ML_2D import Main as DIFF_V_2D
+
+from multiprocessing import Pool
+
+
+# Compute solutions
+p = Pool(4)
+
