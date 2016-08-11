6db0b30b0cca707cc1e071ef52cd18c4b1b2e2b2
Jean-Luc Bouchot <jlbouchot@gmail.com>
2016-08-11 06:34:27 +0200

    Added the non hpc commands to test script

--- CSPG/MLCSPG_finalDraft.py
+++ CSPG/MLCSPG_finalDraft.py
@@ -24,3 +24,15 @@ p.apply_async(PWLD_2D, ("pwld2D_grid2000_var_big_L4", tuple([2000, 2000]), 4, "w
 
 p.close()
 p.join()
+
+# Same calls without using the hpc system:
+DIFF_V("diffML_trig_whtp_d20_g1055_grid2000", 10, tuple([2000]), 4, "whtp", 1.055, 1, "p", 50, 1e-4, None, 2, 4.3)
+DIFF_V("diffML_trig_whtp_d30_g1055_grid2000", 15, tuple([2000]), 4, "whtp", 1.055, 1, "p", 50, 1e-4, None, 2, 4.3)
+DIFF_V("diffML_trig_whtp_d30_g1055_fixedhL40000_L1", 15, tuple([40000]), 1, "whtp", 1.055, 1, "p", 50, 1e-4, None, 2, 4.)
+DIFF_V("diffML_trig_whtp_d30_g1055_fixedhL40000_L2", 15, tuple([20000]), 2, "whtp", 1.055, 2, "p", 50, 1e-4, None, 2, 4.))
+DIFF_V("diffML_trig_whtp_d30_g1055_fixedhL40000_L3", 15, tuple([10000]), 3, "whtp", 1.055, 3, "p", 50, 1e-4, None, 2, 4.3)
+DIFF_V("diffML_trig_whtp_d30_g1055_fixedhL40000_L4", 15, tuple([5000]), 4, "whtp", 1.055, 4, "p", 50, 1e-4, None, 2, 4.3)
+PWLD_V("pwldML_whtp_var_big_L4", tuple([2000]), 4, "whtp", 1.05, 5, 2.0, 1, "p", 50, 1e-4, None)
+PWLD_V("pwldML_whtp_var_small_L4", tuple([2000]), 4, "whtp", 1.05, 5, 1.0/13.0, 1, "p", 50, 1e-4, None)
+PWLD_2D("pwld2D_grid2000_var_small_L4", tuple([2000, 2000]), 4, "whtp", 1.06, 5, 1.0/25.0, 1, "p", 50, 1e-3, None)
+PWLD_2D("pwld2D_grid2000_var_big_L4", tuple([2000, 2000]), 4, "whtp", 1.06, 5, 2.0, 1, "p", 50, 1e-3, None)
