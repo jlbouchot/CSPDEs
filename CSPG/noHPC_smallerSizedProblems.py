83f28a2457457cefdafcd528ffa65612b1dbdc28
Jean-Luc Bouchot <jlbouchot@gmail.com>
2016-09-30 12:09:14 +0200

    Corrected a small mistake in the test file

--- CSPG/noHPC_smallerSizedProblems.py
+++ CSPG/noHPC_smallerSizedProblems.py
@@ -1,3 +1,3 @@
 from test_diff_avg_v_ML import Main as DIFF_V
 
-DIFF_V("diffML_whtp_d12_g105_grid200_L3", 6, tuple([2000]), 3, "whtp", 1.05, "p", 50, 1e-3, 100, 1, 5)
+DIFF_V("diffML_whtp_d12_g105_grid200_L3", 6, tuple([2000]), 3, "whtp", 1.05, 1, "p", 50, 1e-3, 100, 1, 5)
