d9aaffa73d60f985c8e4e760e2ddef9ad192a12c
Jean-Luc Bouchot <jlbouchot@gmail.com>
2017-10-25 17:44:23 +0200

    Still trying to figure out what is going on with this mesh refinement

--- CSPG/test_diff_avg_v_ML.py
+++ CSPG/test_diff_avg_v_ML.py
@@ -68,15 +68,15 @@ def Main(outfile = "thatTest", d = 5, grid_points = tuple([2000]), L_max = 4, al
                                 get_sampling_type(sampling_name), WR.check_cs)
         #wr_model   = WR.WRModel(WR.Algorithms.whtp, WR.Operators.Chebyshev, v,
         #                        WR.cs_pragmatic_m, WR.check_cs) # or cs_theoretic_m
-        print 'wr_model created'
+
 
 		## Number of tests
         num_tests = nb_tests # change from 10 for Quinoa tests
 
-		## Don't forget to reset the original mesh
-        spde_model.refine_mesh(2**(-s))
 		### Execute test
         test_result = test(spde_model, wr_model, nb_iter, epsilon, s, [CrossCheck(num_tests)], dat_constant, *test_result)
+		## Don't forget to reset the original mesh
+        spde_model.refine_mesh(2**(-(s-1)))
 
 
 ### Main
