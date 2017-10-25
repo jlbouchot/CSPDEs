c1c7bec5bcb84f35f2eafefcdd1f1acf44c3c210
Jean-Luc Bouchot <jlbouchot@gmail.com>
2017-10-25 17:48:19 +0200

    Almost there ...

--- CSPG/SPDE/FEniCSModels/FEMModel.py
+++ CSPG/SPDE/FEniCSModels/FEMModel.py
@@ -26,9 +26,9 @@ class FEMModel(SPDEModel):
                 assert False, "Only one to three dimensional problems supported"
         else:
             self.mesh = UnitIntervalMesh(self.mesh_size)
+        print self.mesh_size
 
     def refine_mesh(self, ratio=2): # Note, this can also be used to coarsen the mesh
-        print self.mesh_size
         self.mesh_size = tuple(int(one_direction*ratio) for one_direction in self.mesh_size)
         self.init_simple_mesh()
 
