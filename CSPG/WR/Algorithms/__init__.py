011d6120fb039391984c88ab96587fda8b114f08
Jean-Luc Bouchot <jlbouchot@gmail.com>
2017-10-26 12:54:36 +0200

    Should be okay now with CVX

--- CSPG/WR/Algorithms/__init__.py
+++ CSPG/WR/Algorithms/__init__.py
@@ -18,9 +18,9 @@ __all__ = [
     'womp',
 ]
 
-#from exact_wbp_cvx import *
-#from qc_wbp_cvx import *
-#from Qc_wbp_precond_primaldual import *
+from exact_wbp_cvx import *
+from qc_wbp_cvx import *
+from Qc_wbp_precond_primaldual import *
 from wiht import *
 from whtp import *
 from wcosamp import *
