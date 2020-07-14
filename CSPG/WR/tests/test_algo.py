6221ff7128c9775f5d1cb0fddf905d8dd1b229fd
Falk Pulsmeyer <falk.pulsmeyer@rwth-aachen.de>
2020-07-14 17:26:27 +0200

    current state of the test where first call of whtp causes a segmentation fault in operator from matrix alt

--- CSPG/WR/tests/test_algo.py
+++ CSPG/WR/tests/test_algo.py
@@ -2,13 +2,12 @@ import pytest
 
 import numpy as np
 import WR
-from WR.Algorithms.weighted_quasi_abslargest import *
 from .conftest import *
 
 
 
 
-def test_algorithms(algo, para_set):
+def tes_algorithms(algo, para_set):
     # x_truth = sparse_ground_truth(n)
     x_truth = para_set["x_truth"]
     # op = create_Operator(m,n)
@@ -17,8 +16,29 @@ def test_algorithms(algo, para_set):
     y = para_set["y"]
     w = np.ones(parameter_set["n"])
     eta = para_set["epsilon"]
-    s = int(np.rint(para_set["density"]*para_set["n"]+0.01)) # 0.01 is an ofset because of rounding limitations in numpy.rint
-    res = WR.Algorithms.whtp(op, y, w, s, eta, para_set["maxiter"])
+    s = para_set["s"]
+    res = algo(op, y, w, s, eta, para_set["maxiter"])
+    # print("x_truth: ", x_truth)
+    # print("res[\"x\"]", res[0])
+    assert np.linalg.norm(res[0]-x_truth) <= 1e-10
+
+def test_algorithms_Alt(algo, para_set_Alt):
+    # x_truth = sparse_ground_truth(n)
+    x_truth = para_set_Alt["x_truth"]
+    # op = create_Operator(m,n)
+    op = para_set_Alt["operator"]
+    # y = create_obs_y(op,x_truth)
+    y = para_set_Alt["y"]
+    w = calculate_weights(para_set_Alt["theta"], para_set_Alt["v_weights"], para_set_Alt["J"])
+    print("w")
+    # print(5/0)
+    eta = para_set_Alt["epsilon"]
+    s = int(np.rint(para_set_Alt["density"]*para_set_Alt["n"]+0.01)) # 0.01 is an ofset because of rounding limitations in numpy.rint
+    res = WR.Algorithms.whtp(op, y, w, s, eta, para_set_Alt["maxiter"])
     print("x_truth: ", x_truth)
     print("res[\"x\"]", res[0])
     assert np.linalg.norm(res[0]-x_truth) <= 1e-10
+
+# needs to be imported from CSPDE_ML for testing reasons copied here
+def calculate_weights(theta, v, J_s):
+    return np.array([theta**np.count_nonzero(nu) * np.product(v[np.where(nu > 0)]**nu[np.where(nu > 0)]) for nu in J_s])
