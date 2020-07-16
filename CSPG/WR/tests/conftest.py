b364d4d2d26888fd7338e40301e0d912665cc22d
Falk Pulsmeyer <falk.pulsmeyer@rwth-aachen.de>
2020-07-16 15:16:00 +0200

    working tests at the moment I am still unsure how to choose m properly depending on sparsity and dimension; as well as the comments for initializing the ground truth

--- CSPG/WR/tests/conftest.py
+++ CSPG/WR/tests/conftest.py
@@ -50,6 +50,10 @@ def find_loc(x, dist):
             break
     return i
 
+def calculate_weights(theta, v, J_s):
+    return np.array([theta**np.count_nonzero(nu) * np.product(v[np.where(nu > 0)]**nu[np.where(nu > 0)]) for nu in J_s])
+
+#only useful in the unweighted case
 def sparse_ground_truth(size, density, randstate):
     matrix = rand(size, 1, density=density, format="csr", random_state=randstate).todense()
     return np.squeeze(np.asarray(matrix)) #squeeze needed because rand returns the deprecated matrix and we want to reduce dim
@@ -61,7 +65,7 @@ def create_Operator(m,n,randstate):
     operator = WR.Operators.operator_from_matrix(WR.Operators.Chebyshev, matrix)
     return operator
 
-def create_Operator_Alt(n,randstate):
+def create_Operator_Alt(randstate):
     def base(x, k):
         return np.cos(k * np.arccos(x)) * np.sqrt(2)**(k>0)
     np.random.seed(randstate)
@@ -96,18 +100,37 @@ def para_set(request):
 
 @pytest.fixture(params=np.asarray(range(nb_test_runs))+1)
 def para_set_Alt(request):
+    # initialization of the parameters including the ones already fixed at the top of the file
     parameter_set["randstate"] = request.param
     parameter_set["v_weights"] = np.hstack((np.repeat(parameter_set["v"], parameter_set["d"]), [np.inf]))
+    # fixed the sparsity parameter s at the moment
+    # while s=10 the resulting number of len(J) is 56
+    # I don't understand how the parameter s correlates to the density value
     parameter_set["s"] = 10 #int(np.rint(parameter_set["density"]*parameter_set["n"]+0.01))
     # parameter_set["m"] = int(np.ceil(2 * np.log(parameter_set["s"])**2 * parameter_set["s"] * np.log(parameter_set["n"])))
-    parameter_set["x_truth"] = sparse_ground_truth(parameter_set["n"],
-                                                   parameter_set["density"],
-                                                   parameter_set["randstate"])
-    set_3 = create_Operator_Alt(parameter_set["n"],parameter_set["randstate"])
+    # to create the operator we don't need the parameter n (which is the size of the ground truth) why don't I need it
+    # or where do I have to insert it in the function create_Operator_Alt()?
+    set_3 = create_Operator_Alt(parameter_set["randstate"])
     parameter_set["J"] = set_3["J"]
+    # the problem why the first test did return a segmentation fault and the following test did not was:
+    # here I reset the parameter n based on the size of J
+    # for the first test then x_truth was initialized for the size 1000
+    # then after initialization of x we set it to len(J)
+    # which lead it to initialize x_truth in the secound run with len(J) instead of 1000
     parameter_set["n"] = len(parameter_set["J"])
+    # with this configuration all the test seem to work yet it still holds that m>>n with 427 > 56
+    # I don't know how to properly set or redefine this WR.cs_theoretic_m_new for a good result (m<n)
     parameter_set["m"] = set_3["m"]
     parameter_set["operator"] = set_3["operator"]
+    parameter_set["weights"] = calculate_weights(parameter_set["theta"], parameter_set["v_weights"], parameter_set["J"])
+    parameter_set["true_dim"] = 1000
+    # now I moved the calculation of the ground truth after the initialization process of J so that I have the correct value for n
+    # and instead of the dimension 1000 we are now working on the dimension 56 I think which is why I am confused
+    print("n: ", parameter_set["n"], " m: ", parameter_set["m"])
+    parameter_set["x_truth"] = weighted_sparse_ground_truth(parameter_set["n"],
+                                                    parameter_set["weights"],
+                                                    parameter_set["density"],
+                                                   parameter_set["randstate"])
     parameter_set["y"] = create_obs_y(parameter_set["operator"], parameter_set["x_truth"])
     return parameter_set
 
--- CSPG/WR/tests/test_algo.py
+++ CSPG/WR/tests/test_algo.py
@@ -6,7 +6,7 @@ from .conftest import *
 
 
 
-
+# the name of this function is written wrong on purpose so it is not detected by pytest
 def tes_algorithms(algo, para_set):
     # x_truth = sparse_ground_truth(n)
     x_truth = para_set["x_truth"]
@@ -20,7 +20,7 @@ def tes_algorithms(algo, para_set):
     res = algo(op, y, w, s, eta, para_set["maxiter"])
     # print("x_truth: ", x_truth)
     # print("res[\"x\"]", res[0])
-    assert np.linalg.norm(res[0]-x_truth) <= 1e-10
+    assert np.linalg.norm(res[0]-x_truth) <= 1e-32
 
 def test_algorithms_Alt(algo, para_set_Alt):
     # x_truth = sparse_ground_truth(n)
@@ -29,9 +29,8 @@ def test_algorithms_Alt(algo, para_set_Alt):
     op = para_set_Alt["operator"]
     # y = create_obs_y(op,x_truth)
     y = para_set_Alt["y"]
-    w = calculate_weights(para_set_Alt["theta"], para_set_Alt["v_weights"], para_set_Alt["J"])
-    print("w")
-    # print(5/0)
+    w = para_set_Alt["weights"]
+    # w = calculate_weights(para_set_Alt["theta"], para_set_Alt["v_weights"], para_set_Alt["J"])
     eta = para_set_Alt["epsilon"]
     s = int(np.rint(para_set_Alt["density"]*para_set_Alt["n"]+0.01)) # 0.01 is an ofset because of rounding limitations in numpy.rint
     res = WR.Algorithms.whtp(op, y, w, s, eta, para_set_Alt["maxiter"])
@@ -40,5 +39,3 @@ def test_algorithms_Alt(algo, para_set_Alt):
     assert np.linalg.norm(res[0]-x_truth) <= 1e-10
 
 # needs to be imported from CSPDE_ML for testing reasons copied here
-def calculate_weights(theta, v, J_s):
-    return np.array([theta**np.count_nonzero(nu) * np.product(v[np.where(nu > 0)]**nu[np.where(nu > 0)]) for nu in J_s])
