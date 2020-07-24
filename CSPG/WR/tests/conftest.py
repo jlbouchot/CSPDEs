6b2cd9645443dfd74dfa0e9803451cb85ba8015d
Falk Pulsmeyer <falk.pulsmeyer@rwth-aachen.de>
2020-07-24 15:14:28 +0200

    The Testing setup now includes the following three scenarios: - Original Operator in an UNWEIGHTED scenario - Original Operator in a WEIGHTED scenario - New alternativ Operator in a WEIGHTED Scenario
    The two convex algorithms only work in the Original Scenario because they need access to a fully generated matrix for solving the convex problem.
    The two convex algorithms also seem a lot slower than the other algorithms and the results do not share the accuracy of the other algorithms.
    
    The are main reason for the failing test is that the assertion measures with an accuracy of 1e-10 yet some algorithm only approximate the ground truth up to
    1e-6 or 1e-5. Therefore it might be worth to reduce the accuracy in the assertion point.
    
    Also there seems to be a problem with WIHT in the unweighted case. It diverges every time.
    
    The last thing is that we maybe should change the initialization of the parameter m in the unweighted case because using the function from WR produces an m>>n which is not
    wanted.
    Since the parameter m, n and s correlate we probably have to change s as well and maybe use the ground truth creation function with unweighted parameters.

--- CSPG/WR/tests/conftest.py
+++ CSPG/WR/tests/conftest.py
@@ -4,55 +4,39 @@ import WR
 from scipy.sparse import rand
 from WR.Operators.operator_from_matrix_Alt import univ_tensor_from_tensor_indices
 
-parameter_set = {"epsilon": 1e-6, # success in recovery ?
-                 "density": 0.05, # proportion of non zero components
-                 "n": 1000, # UNWEIGHTED ONLY Size of the ambient space, not in the parametric PDE like framework
-                 "maxiter": 100, # maximum number of iterations for the iterative algorithms
-                 "d": 30, # WEIGHTED ONLY number of _virtual_ operators
-                 "theta": np.sqrt(2), # WEIGHTED ONLY l-infinity norm of the univariate operators (see MLCSPG paper)
-                 "v" : 1.05 # WEIGHTED ONLY Uniform weights for the operators
-                 }
+parameter_set_test_config = {"epsilon": 1e-6,  # success in recovery ?
+                             "density": 0.05,  # proportion of non zero components
+                             "n": 1000,
+                             # UNWEIGHTED ONLY Size of the ambient space, not in the parametric PDE like framework
+                             "maxiter": 100,  # maximum number of iterations for the iterative algorithms
+                             "d": 30,  # WEIGHTED ONLY number of _virtual_ operators
+                             "theta": np.sqrt(2),
+                             # WEIGHTED ONLY l-infinity norm of the univariate operators (see MLCSPG paper)
+                             "v": 1.05,  # WEIGHTED ONLY Uniform weights for the operators
+                             "s": 15  # will be OVERWRITTEN in UNWEIGHTED Case
+                             }
+nb_test_runs = 6 # number of tests
 
-# >>> ws = np.array([1,1.4, 2.1,3,5])
-# >>> ws
-# array([1. , 1.4, 2.1, 3. , 5. ])
-# >>> winv = ws**(-1)
-# >>> winv
-# array([1. , 0.71428571, 0.47619048, 0.33333333, 0.2 ])
-# >>> cumDistribution = np.cumsum(winv)
-# >>> cumDistribution
-# array([1. , 1.71428571, 2.19047619, 2.52380952, 2.72380952])
-# >>> cumDistribution = cumDistribution / cumDistribution[-1]
-# >>> cumDistribution
-# array([0.36713287, 0.62937063, 0.8041958 , 0.92657343, 1. ])
-# >>> np.random.rand()
-# 0.49981139949214004
-
-
-# randstate = 42
-# m = int(np.log(n) * density * n)
-nb_test_runs = 2
 
 def weighted_sparse_ground_truth(size, weights, s, randstate):
-    np.random.seed(randstate)
-    cumDist = np.cumsum(weights**(-1))
-    cumDist = cumDist/cumDist[-1]
-    truth = np.zeros(size)
-    # sampling with replacement
-    # TODO: dont replace 
+    np.random.seed(randstate)  # set randomstate for reproducibility
+    cumDist = np.cumsum(weights ** (-1))
+    cumDist = cumDist / cumDist[-1]
+    truth = np.zeros(size)  # initialize ground truth
     weighted_l0_norm = 0
+    loc_set = set()  # set to remember the used locations
     while weighted_l0_norm < s:
-        rand_loc = find_loc(np.random.rand(1)[0],cumDist)
+        rand_loc = find_loc(np.random.rand(1)[0], cumDist)
+        while rand_loc in loc_set:
+            rand_loc = find_loc(np.random.rand(1)[0], cumDist)
+        loc_set.add(rand_loc)
         curWeight = weights[rand_loc]
         print("Position {} with weight {}".format(rand_loc, curWeight))
-        weighted_l0_norm = weighted_l0_norm + curWeight**2
-        truth[rand_loc] = np.random.normal()/curWeight #why divide through the weight?
-        truth[rand_loc] = 1.0/curWeight
-    # for i in range(0,int(size*density)):
-    #     rand_loc = find_loc(np.random.rand(1)[0],cumDist) # because random.rand returns an array
-    #     truth[rand_loc] = np.random.normal()/weights[rand_loc]
-    #     truth[rand_loc] = 1.0/weights[rand_loc]
+        weighted_l0_norm = weighted_l0_norm + curWeight ** 2
+        truth[rand_loc] = np.random.normal() / curWeight  # why divide through the weight?
+        # truth[rand_loc] = 1.0/curWeight
     return truth
+
 
 def find_loc(x, dist):
     for i in range(0, len(dist)):
@@ -60,102 +44,126 @@ def find_loc(x, dist):
             break
     return i
 
-def calculate_weights(theta, v, J_s):
-    return np.array([theta**np.count_nonzero(nu) * np.product(v[np.where(nu > 0)]**nu[np.where(nu > 0)]) for nu in J_s])
 
-#only useful in the unweighted case
-def sparse_ground_truth(size, density, randstate): # Only in the unweighted case
+# UNWEIGHTED ONLY
+def sparse_ground_truth(size, density, randstate):  # Only in the unweighted case
     matrix = rand(size, 1, density=density, format="csr", random_state=randstate).todense()
-    return np.squeeze(np.asarray(matrix)) #squeeze needed because rand returns the deprecated matrix and we want to reduce dim
+    return np.squeeze(
+        np.asarray(matrix))  # squeeze needed because rand returns the deprecated matrix and we want to reduce dim
 
-def create_Operator(m,n,randstate): # Only in the unweighted case
+# Original Operator
+def create_Operator(m, n, randstate):  # Only in the unweighted case
     np.random.seed(randstate)
     # Create a random Gaussian matrix, which is known to fulfill (unweighted)RIP with high probability
-    matrix = np.random.normal(size=(m,n))
-    matrix = matrix/(np.sqrt(m))
+    matrix = np.random.normal(size=(m, n))
+    matrix = matrix / (np.sqrt(m))
     operator = WR.Operators.operator_from_matrix(WR.Operators.Chebyshev, matrix)
     return operator
 
-def create_Operator_Alt(randstate): # Only for the weighted case
+# new Operator
+def create_Operator_Alt(para_set):  # Only for the weighted case
     def base(x, k):
-        return np.cos(k * np.arccos(x)) * np.sqrt(2)**(k>0)
+        return np.cos(k * np.arccos(x)) * np.sqrt(2) ** (k > 0)
+    randstate = para_set["randstate"]
     np.random.seed(randstate)
-    J = calc_J(parameter_set["s"], parameter_set["theta"], parameter_set["v_weights"])
-    m = WR.cs_theoretic_m_new(parameter_set["s"], len(J))
-    d = parameter_set["d"]
+    J = calc_J(para_set["s"], para_set["theta"], para_set["v_weights"])
+    m = WR.cs_theoretic_m_new(para_set["s"], len(J))
+    d = para_set["d"]
     Z = WR.Operators.LD_bounded_operator.apply_precondition_measure(np.random.uniform(-1, 1, (m, d)))
-    operator = WR.Operators.operator_from_matrix_Alt(WR.Operators.Cheb_Alt, np.zeros((2,2)),
+    operator = WR.Operators.operator_from_matrix_Alt(WR.Operators.Cheb_Alt, np.zeros((2, 2)),
                                                      univ_tensor_from_tensor_indices(J, Z, base), J)
     return {"operator": operator, "J": J, "Z": Z, "m": m}
 
-def create_obs_y(op,x):
+
+def create_obs_y(op, x):
     return op.apply(x)
 
-
-@pytest.fixture(params=np.asarray(range(nb_test_runs))+14)
-def para_set(request):
+@pytest.fixture(params=np.asarray(range(nb_test_runs)) + 32)
+def para_set_orig_unw(request):
+    parameter_set = dict(parameter_set_test_config)
     parameter_set["randstate"] = request.param
-    parameter_set["s"] = int(np.rint(parameter_set["density"]*parameter_set["n"]+0.01))
-    # parameter_set["m"] = WR.cs_theoretic_m_new(parameter_set["s"], parameter_set["n"])
-    parameter_set["m"] = int(np.ceil(2 * np.log10(parameter_set["s"])**2 * parameter_set["s"] * np.log10(parameter_set["n"])))
-    # parameter_set["m"] = int(np.ceil(2 * np.log(parameter_set["s"])**2 * np.log(parameter_set["n"])))
+    parameter_set["s"] = int(parameter_set["n"] * parameter_set["density"])
     parameter_set["x_truth"] = sparse_ground_truth(parameter_set["n"],
                                                    parameter_set["density"],
                                                    parameter_set["randstate"])
+    # TODO: better definition of m in the unweighted case
+    parameter_set["m"] = int(.25*parameter_set["n"])
+    # the below one returns m >> n
+    # parameter_set["m"] = WR.cs_theoretic_m_new(parameter_set["s"], parameter_set["n"])
+    print("n: {0}, m: {1}".format(parameter_set["n"], parameter_set["m"]))
     parameter_set["operator"] = create_Operator(parameter_set["m"],
                                                 parameter_set["n"],
                                                 parameter_set["randstate"])
     parameter_set["y"] = create_obs_y(parameter_set["operator"], parameter_set["x_truth"])
     return parameter_set
 
-
-@pytest.fixture(params=np.asarray(range(nb_test_runs))+1)
-def para_set_Alt(request):
-    # initialization of the parameters including the ones already fixed at the top of the file
+@pytest.fixture(params=np.asarray(range(nb_test_runs)) + 32)
+def para_set_orig_wei(request):
+    parameter_set = dict(parameter_set_test_config)
     parameter_set["randstate"] = request.param
     parameter_set["v_weights"] = np.hstack((np.repeat(parameter_set["v"], parameter_set["d"]), [np.inf]))
-    # fixed the sparsity parameter s at the moment
-    # while s=10 the resulting number of len(J) is 56
-    # I don't understand how the parameter s correlates to the density value
-    parameter_set["s"] = 20 #int(np.rint(parameter_set["density"]*parameter_set["n"]+0.01))
-    # parameter_set["m"] = int(np.ceil(2 * np.log(parameter_set["s"])**2 * parameter_set["s"] * np.log(parameter_set["n"])))
-    # to create the operator we don't need the parameter n (which is the size of the ground truth) why don't I need it
-    # or where do I have to insert it in the function create_Operator_Alt()?
-    set_3 = create_Operator_Alt(parameter_set["randstate"])
-    parameter_set["J"] = set_3["J"]
-    # the problem why the first test did return a segmentation fault and the following test did not was:
-    # here I reset the parameter n based on the size of J
-    # for the first test then x_truth was initialized for the size 1000
-    # then after initialization of x we set it to len(J)
-    # which lead it to initialize x_truth in the secound run with len(J) instead of 1000
+    np.random.seed(parameter_set["randstate"])
+    parameter_set["J"] = calc_J(parameter_set["s"], parameter_set["theta"], parameter_set["v_weights"])
     parameter_set["n"] = len(parameter_set["J"])
-    # with this configuration all the test seem to work yet it still holds that m>>n with 427 > 56
-    # I don't know how to properly set or redefine this WR.cs_theoretic_m_new for a good result (m<n)
-    parameter_set["m"] = set_3["m"]
-    parameter_set["operator"] = set_3["operator"]
+    parameter_set["m"] = WR.cs_theoretic_m_new(parameter_set["s"], len(parameter_set["J"]))
+    print("n: {0}, m: {1}".format(parameter_set["n"], parameter_set["m"]))
+    parameter_set["operator"] = create_Operator(parameter_set["m"],
+                                                parameter_set["n"],
+                                                parameter_set["randstate"])
     parameter_set["weights"] = calculate_weights(parameter_set["theta"], parameter_set["v_weights"], parameter_set["J"])
-    parameter_set["true_dim"] = 1000
-    # now I moved the calculation of the ground truth after the initialization process of J so that I have the correct value for n
-    # and instead of the dimension 1000 we are now working on the dimension 56 I think which is why I am confused
-    print("n: ", parameter_set["n"], " m: ", parameter_set["m"])
     parameter_set["x_truth"] = weighted_sparse_ground_truth(parameter_set["n"],
-                                                    parameter_set["weights"],
-                                                    parameter_set["s"],
-                                                   parameter_set["randstate"])
+                                                            parameter_set["weights"],
+                                                            parameter_set["s"],
+                                                            parameter_set["randstate"])
     parameter_set["y"] = create_obs_y(parameter_set["operator"], parameter_set["x_truth"])
     return parameter_set
 
+
+@pytest.fixture(params=np.asarray(range(nb_test_runs)) + 1)
+def para_set_Alt(request):
+    parameter_set = dict(parameter_set_test_config)  # explicitly copy the basic test configuration from above
+    # initialization of the parameters
+    parameter_set["randstate"] = request.param
+    parameter_set["v_weights"] = np.hstack((np.repeat(parameter_set["v"], parameter_set["d"]), [np.inf]))
+    set_3 = create_Operator_Alt(parameter_set)
+    parameter_set["J"] = set_3["J"]
+    parameter_set["n"] = len(parameter_set["J"])
+    parameter_set["m"] = set_3["m"]
+    parameter_set["operator"] = set_3["operator"]
+    parameter_set["weights"] = calculate_weights(parameter_set["theta"], parameter_set["v_weights"], parameter_set["J"])
+    print("n: ", parameter_set["n"], " m: ", parameter_set["m"])
+    parameter_set["x_truth"] = weighted_sparse_ground_truth(parameter_set["n"],
+                                                            parameter_set["weights"],
+                                                            parameter_set["s"],
+                                                            parameter_set["randstate"])
+    parameter_set["y"] = create_obs_y(parameter_set["operator"], parameter_set["x_truth"])
+    return parameter_set
+
+
 @pytest.fixture(params=[WR.Algorithms.whtp, WR.Algorithms.wiht,
-                        WR.Algorithms.womp])#,
-                        # WR.Algorithms.exact_wbp_cvx,
-                        # WR.Algorithms.qc_wbp_cvx])
-#                         WR.Algorithms.Qc_wbp_precond_primaldual, WR.Algorithms.wcosamp,
-#                         , , WR.Algorithms.primaldual])
+                        WR.Algorithms.womp, WR.Algorithms.exact_wbp_cvx,
+                        WR.Algorithms.qc_wbp_cvx, WR.Algorithms.wcosamp])
+def algo_orig(request):
+    return request.param
+
+
+@pytest.fixture(params=[WR.Algorithms.whtp, WR.Algorithms.wiht,
+                        WR.Algorithms.womp,
+                        WR.Algorithms.wcosamp])
 def algo(request):
     return request.param
 
-# needs to be imported from CSPDE_ML but we need to write an appropriate init file for CSPG to import from there
+
+# the following functions needs to be imported from CSPDE_ML but we need to write an appropriate init file for CSPG to import from there
+
+def calculate_weights(theta, v, J_s):
+    return np.array(
+        [theta ** np.count_nonzero(nu) * np.product(v[np.where(nu > 0)] ** nu[np.where(nu > 0)]) for nu in J_s])
+
+
 import itertools
+
+
 def calc_J(s, theta, v):
     def iterate(M, a, B, S):
         def iterate_(B, S, p):
@@ -164,14 +172,13 @@ def calc_J(s, theta, v):
             if len(S):
                 while True:
                     # Take the highest index in S
-                    r     = S[-1]
+                    r = S[-1]
 
                     # Substract weight from B
-                    B    -= a[r]
+                    B -= a[r]
 
                     # Increase nu_r by one
                     p[r] += 1
-
 
                     if len(S) > 1:
                         # If there is more than one index left, recurse with remaining indices and 'remaining weight' B
@@ -195,17 +202,16 @@ def calc_J(s, theta, v):
 
         return iterate_(B, S, np.zeros(M, dtype='int'))
 
-
     # Set A and a as in Theorem 5.2
-    A = np.log2(s/2.)
+    A = np.log2(s / 2.)
     a = 2 * np.log2(v)
     T = 2 * np.log2(theta)
 
     # Determine maximal M s.t. for j = 0 ... M-1 is a_j <= A - T
     # M is also the maximal support size
     # M = np.argmin(a <= A - T)
-    M = np.argmin(a <= A )
-    assert 0 != M, "Weight array too short. (Last element: {0}. Threshold: {1})".format(a[-1], A-T)
+    M = np.argmin(a <= A)
+    assert 0 != M, "Weight array too short. (Last element: {0}. Threshold: {1})".format(a[-1], A - T)
 
     # If A is non-negative the zero vector is always admissible
     assert A >= 0, "Negative A, i.e. sparsity less than 2."
@@ -216,7 +222,7 @@ def calc_J(s, theta, v):
         new_indices = []
 
         for S in itertools.combinations(range(M), k):
-            new_indices += iterate(M, a, A - k*T, list(S))
+            new_indices += iterate(M, a, A - k * T, list(S))
 
         if [] == new_indices:
             break
--- CSPG/WR/tests/test_algo.py
+++ CSPG/WR/tests/test_algo.py
@@ -6,24 +6,41 @@ from .conftest import *
 
 
 
-# the name of this function is written wrong on purpose so it is not detected by pytest
-def tes_algorithms(algo, para_set):
+def tes_algorithms_Orig_unweighted(algo_orig, para_set_orig_unw):
     # x_truth = sparse_ground_truth(n)
-    x_truth = para_set["x_truth"]
+    x_truth = para_set_orig_unw["x_truth"]
     # op = create_Operator(m,n)
-    op = para_set["operator"]
+    op = para_set_orig_unw["operator"]
     # y = create_obs_y(op,x_truth)
-    y = para_set["y"]
-    w = np.ones(parameter_set["n"])
-    eta = para_set["epsilon"]
-    s = para_set["s"]
-    res = algo(op, y, w, s, eta, para_set["maxiter"])
+    y = para_set_orig_unw["y"]
+    w = np.ones(para_set_orig_unw["n"])
+    eta = para_set_orig_unw["epsilon"]
+    s = para_set_orig_unw["s"]
+    res = algo_orig(op, y, w, s, eta, para_set_orig_unw["maxiter"])
     # print("x_truth: ", x_truth)
     # print("res[\"x\"]", res[0])
     assert np.linalg.norm(res[0]-x_truth) <= 1e-10
 
-def test_algorithms_Alt(algo, para_set_Alt):
+
+def test_algorithms_Orig_weighted(algo_orig, para_set_orig_wei):
     # x_truth = sparse_ground_truth(n)
+    x_truth = para_set_orig_wei["x_truth"]
+    # op = create_Operator(m,n)
+    op = para_set_orig_wei["operator"]
+    # y = create_obs_y(op,x_truth)
+    y = para_set_orig_wei["y"]
+    # TODO: fix weight
+    w = np.ones(para_set_orig_wei["n"])
+    eta = para_set_orig_wei["epsilon"]
+    s = para_set_orig_wei["s"]
+    res = algo_orig(op, y, w, s, eta, para_set_orig_wei["maxiter"])
+    # print("x_truth: ", x_truth)
+    # print("res[\"x\"]", res[0])
+    assert np.linalg.norm(res[0]-x_truth) <= 1e-6
+
+
+def test_algorithms_Alt_weighted(algo, para_set_Alt):
+    # x_trut = sparse_ground_truth(n)
     x_truth = para_set_Alt["x_truth"]
     # op = create_Operator(m,n)
     op = para_set_Alt["operator"]
@@ -33,10 +50,8 @@ def test_algorithms_Alt(algo, para_set_Alt):
     # w = calculate_weights(para_set_Alt["theta"], para_set_Alt["v_weights"], para_set_Alt["J"])
     eta = para_set_Alt["epsilon"]
     # s = int(np.rint(para_set_Alt["density"]*para_set_Alt["n"]+0.01)) # 0.01 is an ofset because of rounding limitations in numpy.rint
-    s = parameter_set["s"]
+    s = para_set_Alt["s"]
     res = algo(op, y, w, s, eta, para_set_Alt["maxiter"])
     # print("x_truth: ", x_truth)
     # print("res[\"x\"]", res[0])
-    assert np.linalg.norm(res[0]-x_truth) <= 1e-10
-
-# needs to be imported from CSPDE_ML for testing reasons copied here
+    assert np.linalg.norm(res[0]-x_truth) <= 1e-6
