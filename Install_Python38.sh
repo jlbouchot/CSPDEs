013fdcedcc371b877f1e9e79dbfb6929a91bade2
Jean-Luc Bouchot <jlbouchot@gmail.com>
2024-04-23 18:51:52 +0200

    Adding information for installing on various python3 versions

---
+++ Install_Python38.sh
@@ -0,0 +1,9 @@
+conda create -n Py38CSPDE python=3.8
+conda activate Py38CSPDE
+conda install -c conda-forge fenics=2019.1.0
+conda install numba=0.51.2
+conda install matplotlib=3.7.2
+conda install progressbar2=4.2.0
+conda install -c conda-forge cvxpy=1.4.2
+conda install -c conda-forge cvxopt=1.3.2
+conda install numpy=1.23.5
---
+++ Install_Python39.sh
@@ -0,0 +1,8 @@
+conda create -n Py39CSPDE python=3.9
+conda activate Py39CSPDE
+conda install -c conda-forge fenics=2019.1.0
+conda install numba=0.51.2
+conda install matplotlib=3.7.2
+conda install progressbar2=4.2.0
+conda install -c conda-forge cvxpy=1.4.2
+conda install -c conda-forge cvxopt=1.3.2
