c016744f1863635eea474ad9e39e5087d3b6f412
Jean-Luc Bouchot <jlbouchot@gmail.com>
2025-10-02 16:08:51 +0200

    PRELIMINARY WORK: No guarantee!

---
+++ InstallTest.sh
@@ -0,0 +1,4 @@
+python test_wCosine_avg_v_ML.py -d 5 -o WeightedCosine1D -L 4 -s 0 -m 100 -N 20 -r whtp -f CSPDEsTEST -n 200
+python test_wCosine_avg_v_ML.py -d 5 -o WeightedCosine1D -L 4 -s 0 -m 100 -N 20 -r qcqp -f CSPDEsTEST_CVX -n 200
+# Add polynomial weights 
+# python test_wCosine_avg_v_ML.py -d 5 -o WeightedCosine1D -L 4 -s 0 -m 100 -N 20 -r qcqp -f CSPDEsTEST_CVX -n 200
